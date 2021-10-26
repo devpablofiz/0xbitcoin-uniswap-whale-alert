72da1c290998b0b8706a7880ae9ded4f92165981
Fappablo <thefappachannelita@gmail.com>
2021-10-26 11:22:02 +0200

    commented config.js

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
-module.exports.poolAddress = "0xaFF587846a44aa086A6555Ff69055D3380fD379a";
-module.exports.blockStep = 2;
-module.exports.blockTimeMS = 13230;
-module.exports.alchemyKey = "yourkey";
-module.exports.minValueForAlert = 2000;
-module.exports.eventsBackupFile = "eventsBackup.csv";
-module.exports.latestBlockBackupFile = "blockBackup.txt";
+module.exports.poolAddress = "0xaFF587846a44aa086A6555Ff69055D3380fD379a"; //the address of the liquidity pool you wish to track (0xaFF587846a44aa086A6555Ff69055D3380fD379a -> 0xBTC / wETH UniswapV3)
+module.exports.blockStep = 2; //blocks to process per cycle
+module.exports.blockTimeMS = 13230; //average time before an ethereum block is mined
+module.exports.alchemyKey = "yourkey"; //alchemy https://dashboard.alchemyapi.io/
+module.exports.minValueForAlert = 2000; //USD threshold value of the trade
+module.exports.eventsBackupFile = "eventsBackup.csv"; //file to backup events to
+module.exports.latestBlockBackupFile = "blockBackup.txt"; //file to backup latest block processed
+
+//contract abi for the liquidity pool (only need the swap event)
 module.exports.poolAbi = [{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":true,"internalType":"address","name":"recipient","type":"address"},{"indexed":false,"internalType":"int256","name":"amount0","type":"int256"},{"indexed":false,"internalType":"int256","name":"amount1","type":"int256"},{"indexed":false,"internalType":"uint160","name":"sqrtPriceX96","type":"uint160"},{"indexed":false,"internalType":"uint128","name":"liquidity","type":"uint128"},{"indexed":false,"internalType":"int24","name":"tick","type":"int24"}],"name":"Swap","type":"event"}];
